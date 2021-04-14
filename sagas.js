fc93faae7dc093f249ece629cf09a84874ac24b8
vipulchakravarthy <saivipul15@msitprogram.net>
2021-04-14 17:49:35 +0530

    test cases completed

--- sagas.js
+++ sagas.js
@@ -1,13 +1,13 @@
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 
-const delay = (ms) => new Promise(res => setTimeout(res, ms))
+export const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
 function* helloSaga() {
     console.log('Hello Sagas!')
 }
 
-function* incrementAsync() {
-    yield delay(1000)
+export function* incrementAsync() {
+    yield call(delay, 1000)
     yield put({ type: 'INCREMENT' })
 }
 
